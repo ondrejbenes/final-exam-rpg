1822a0761d4d95970899ee221e85cac0c194b58f
Ondrejb <ondrejb@stud.ntnu.no>
2016-09-07 13:32:47 +0200

    Animated player character added.

--- imt3601_shineydonkeys/Main.cpp
+++ imt3601_shineydonkeys/Main.cpp
@@ -3,24 +3,83 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
 int main(int argc, char* argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	enum Direction { Down, Left, Right, Up };
+	sf::Vector2i source(0, Down);
+
+	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+
+	sf::Texture playerTexture;
+	sf::Sprite playerImage;
+
+	sf::Clock clock;
+
+	bool updateFrame = true;
+
+	if (!playerTexture.loadFromFile("Resources/Images/Player.png"))
+		std::cout << "Error: could not load player image" << std::endl;
+
+	playerImage.setTexture(playerTexture);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
+			{
+			case sf::Event::Closed:
 				window.close();
+				break;
+			default:
+				break;
+			}
 		}
 
-		window.clear();
-		window.draw(shape);
+		bool keyPressed = true;
+		float speed = 5;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			source.y = Up;
+			playerImage.move(0, -1 * speed);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			source.y = Down;
+			playerImage.move(0, 1 * speed);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			source.y = Right;
+			playerImage.move(1 * speed, 0);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			source.y = Left;
+			playerImage.move(-1 * speed, 0);
+		}
+		else {
+			keyPressed = false;
+		}
+
+		if (keyPressed)
+		{
+			source.x++;
+			if (source.x * 32 >= playerTexture.getSize().x)
+				source.x = 0;
+		}
+
+		playerImage.setTextureRect(sf::IntRect(source.x * 32, source.y * 48, 32, 48));
+
+		window.draw(playerImage);
 		window.display();
+		window.clear();
+
+		sf::sleep(sf::milliseconds(75));
 	}
 
 	return 0;
