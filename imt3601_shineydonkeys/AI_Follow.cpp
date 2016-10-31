1bbbe70feb66fc57cf2e53f164f195dcf7ee9862
Gytis Apinys <gytis.apinys@stud.vgtu.lt>
2016-10-31 19:05:04 +0200

    Testing how velocity works while changing states

--- imt3601_shineydonkeys/AI_Follow.cpp
+++ imt3601_shineydonkeys/AI_Follow.cpp
@@ -1,12 +1,50 @@
-/*#include "AI_Follow.h"
+#include "AI_Follow.h"
+#include "AiComponent.h"
+#include "PhysicsComponent.h"
+#include "AI_Patrol.h"
 
 
-
-AiFollow::AiFollow()
+AiFollow::AiFollow(AiComponent* component) : AiState(component)
 {
 }
 
 
 AiFollow::~AiFollow()
 {
-} */
+} 
+
+void AiFollow::update()
+{
+	auto pc = _AiComponent->getParent().getComponent<PhysicsComponent>();
+	//_AiComponent->ChangeState(new AiFollow(_AiComponent));
+	//pc->move();
+
+	{
+		//int randVelocities[] = { -PhysicsComponent::defaultVelocity.x, 0, PhysicsComponent::defaultVelocity.x };
+		sf::Vector2f velocity;
+		//velocity.x = randVelocities[rand() % 3];
+		//velocity.y = randVelocities[rand() % 3];
+		//if (rand() % 2)
+		//	velocity.x = 0;
+		//else
+		//	velocity.y = 0;
+		velocity.x =  100;
+		velocity.y = -100;
+
+		pc->setVelocity(velocity);
+		framesInOneDirection = 0;
+	}
+
+
+	// testing how velocity works
+	if (test > 100)
+	{
+		_AiComponent->ChangeState(new AiPatrol(_AiComponent));
+		test = 0;
+	}
+	else test++;
+
+	framesInOneDirection++;
+
+
+}
--- imt3601_shineydonkeys/AI_Follow.h
+++ imt3601_shineydonkeys/AI_Follow.h
@@ -1,10 +1,15 @@
-/*#pragma once
+#pragma once
 #include "AI_State.h"
 class AiFollow :
 	public AiState
 {
 public:
-	AiFollow();
+	AiFollow(AiComponent* component);
 	~AiFollow();
+	void update() override;
+private:
+	// TODO remove when we have smarter AI
+	int framesInOneDirection = 0;
+	int test = 0;
 };
-*/
+
